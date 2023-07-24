2feaf8c8be6953fc0f63800acb67e134e0968dd6
Miriam Nussbacher <miriamnussbacher@gmail.com>
2023-07-24 23:45:38 +0300

    add new controller

---
+++ GitTestB2/Controllers/GitController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GitTestB2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitController : ControllerBase
+    {
+        // GET: api/<GitController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GitController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GitController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GitController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GitController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
